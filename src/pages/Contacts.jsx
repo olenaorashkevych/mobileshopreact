72ad6cbdb76c7a8214fba34489df0930305558a4
Olena <olenaorashkevych@gmail.com>
2024-08-19 13:47:39 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -4,8 +4,7 @@ function Contacts() {
     return (
         <div class="article">
             <div class="container">
-                <h2 class="article__title">Контакти 222</h2>
-
+                <h2 class="article__title">Контакти 333</h2>
                 <div class="page-content">
                     <p>Якщо вам потрібна допомога або у вас є запитання, будь ласка, зв'яжіться з нами за наступними
                         контактами:</p>
